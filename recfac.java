56a15975751290633bc9e7973a42bae6df780f83
Rakesh1408 <86716761+Rakesh1408@users.noreply.github.com>
2024-06-07 11:41:58 +0530

    factorial using recursion
    easy and best way, just think about base case and recursive equation.

---
+++ recfac.java
@@ -0,0 +1,11 @@
+public class sum{
+  static int recsum(int n){
+     if(n==1){
+        return 1;
+      }
+    return n*recsum(n-1);
+}
+ public static main(String args[]){
+     int k=3;
+     System.out.println(recsum(k));
+}
